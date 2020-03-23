1c33e7cae7638aecb279ab15187f84bcee506e43
zhubin <zhubin@anyconnect.tech>
2020-03-23 17:23:46 +0800

    thrid commit

--- main.c
+++ main.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 int main(void)
 {
-    printf("hello git-start!\");
+    printf("hello git-start!\n");
     printf("go");
+    printf("feature/some-feature\n");
     return 0;
 }

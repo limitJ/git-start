0bc899c89af7155471419e66500089ce7495a58b
zhubin <zhubin@anyconnect.tech>
2020-03-24 09:56:30 +0800

    success

--- main.c
+++ main.c
@@ -2,7 +2,7 @@
 int main(void)
 {
     printf("hello git-start!\n");
-    printf("go feature some 2");
-    printf("feature/some-feature 2\n");
+    printf("go feature some");
+    printf("feature/some-feature\n");
     return 0;
 }
